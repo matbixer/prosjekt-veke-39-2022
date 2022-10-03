d741032cebd1f7a3d16b093ccfd8380fa721f87e
matbixer <114401592+matbixer@users.noreply.github.com>
2022-10-03 12:47:26 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-serial.writeLine("hallo")
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Hallo")
+})
+radio.onReceivedString(function (receivedString) {
+    Shifra = receivedString
+    basic.showString(Shifra)
+    Shifra += 1
+})
+let Shifra = ""
+radio.setGroup(144)
